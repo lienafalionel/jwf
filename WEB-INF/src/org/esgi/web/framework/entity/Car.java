432336ff51077ac22956692dc8a7ceeb5df44033
lienafalionel <lienafa.lionel@gmail.com>
2015-06-03 11:23:28 +0200

    Classe Car

---
+++ WEB-INF/src/org/esgi/web/framework/entity/Car.java
@@ -0,0 +1,95 @@
+package org.esgi.web.framework.entity;
+
+import javax.persistence.Entity;
+
+public class Car {
+
+	private int id;
+	private String name;
+	private float price;
+	private String year;
+	private String type_fuel;
+	private String gearbox_type;
+	private String description;
+	private String telephone_number;
+	private String email;
+	
+	public Car() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getType_fuel() {
+		return type_fuel;
+	}
+
+	public void setType_fuel(String type_fuel) {
+		this.type_fuel = type_fuel;
+	}
+
+	public String getGearbox_type() {
+		return gearbox_type;
+	}
+
+	public void setGearbox_type(String gearbox_type) {
+		this.gearbox_type = gearbox_type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTelephone_number() {
+		return telephone_number;
+	}
+
+	public void setTelephone_number(String telephone_number) {
+		this.telephone_number = telephone_number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
